d298cc575eb95702376f4e0c873c88ae40e08f31
nobadag <nobaan.dag@gmail.com>
2019-01-12 13:34:45 +0900

    「新規」を押すと、アカウント名入力のシーンを出すように変更。

--- Manager.java
+++ Manager.java
@@ -12,6 +12,8 @@ public class Manager extends Application {
   }
 
   public void start(Stage stage) throws Exception {
+    stage.setTitle("Exam-manager");
+
     Label wel = new Label("Exam-manager にようこそ\n");
     Button nw = new Button("新規");
     Button op = new Button("開く");
@@ -21,7 +23,7 @@ public class Manager extends Application {
     nw.setFont(new Font(20));
     op.setFont(new Font(20));
 
-    BorderPane bp = new BorderPane();
+    BorderPane bp1 = new BorderPane();
     VBox home = new VBox(20);
 
     home.getChildren().add(wel);
@@ -29,13 +31,32 @@ public class Manager extends Application {
     home.getChildren().add(op);
 
     home.setAlignment(Pos.CENTER);
-    bp.setCenter(home);
+    bp1.setCenter(home);
 
-    Scene welcome = new Scene(bp, 600, 400);
+    Scene welcome = new Scene(bp1, 600, 400);
 
     stage.setScene(welcome);
 
-    stage.setTitle("Exam-manager");
+    Label acc = new Label("アカウント名を入力してください。");
+    TextField tf = new TextField();
+
+    acc.setFont(new Font(20));
+
+    BorderPane bp2 = new BorderPane();
+    VBox acvb = new VBox(20);
+
+    acvb.getChildren().add(acc);
+    acvb.getChildren().add(tf);
+
+    acvb.setAlignment(Pos.CENTER);
+
+    bp2.setCenter(acvb);
+
+    Scene make_acc = new Scene(bp2, 600, 400);
+    nw.setOnAction(event -> {
+      stage.setScene(make_acc);
+    });
+
     stage.show();
   }
 }
