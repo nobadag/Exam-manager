fec9af452163134ac8539de8b3af9b7e3a5da073
nobadag <nobaan.dag@gmail.com>
2019-01-19 21:06:29 +0900

    クラスSubjectに教科名を設定するコンクトラクタとゲッターを設定。

--- User.java
+++ User.java
@@ -22,10 +22,18 @@ class User {
 }
 
 class Subject {
-  public String name;
+  private String name;
   private ArrayList<Integer> scores = new ArrayList<>();
+
+  Subject(String tmp) {
+    name = tmp;
+  }
 
   void addScore(int tmp) {
     scores.add(tmp);
+  }
+
+  String getName() {
+    return name;
   }
 }
