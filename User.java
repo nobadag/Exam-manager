177b6c5906a3c82099c91308969ac06f790539e0
nobadag <nobaan.dag@gmail.com>
2019-01-30 23:00:14 +0900

    User.javaに試験のクラス「Exam」を追加。ゲッターとセッターを作成。

--- User.java
+++ User.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class User {
   private String name;
   private String password;
@@ -16,6 +18,27 @@ class User {
 
   public String getPassword() {
     return password;
+  }
+}
+
+class Exams {
+  private String name;
+  private ArrayList<Subject> subsdata = new ArrayList<>();
+
+  public void setName(String tmp) {
+    name = tmp;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setData(ArrayList<Subject> tmp) {
+    subsdata.addAll(tmp);
+  }
+
+  public ArrayList<Subject> getData() {
+    return subsdata;
   }
 }
 
